04d171e8bc8c4b030865aa49714a628ff568ae65
Ziedelth <watteaupierre@gmail.com>
2022-11-18 17:23:22 +0100

    Fix bugs

--- src/main/kotlin/fr/ziedelth/twitter/listeners/EpisodesRelease.kt
+++ src/main/kotlin/fr/ziedelth/twitter/listeners/EpisodesRelease.kt
@@ -7,12 +7,28 @@ import twitter4j.StatusUpdate
 import twitter4j.Twitter
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.net.URI
 import java.net.URL
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.util.*
 import javax.imageio.ImageIO
 
 class EpisodesRelease(private val twitter: Twitter) : Listener {
-    private fun getTinyUrl(url: String?) = URL("https://urlz.fr/api_new.php?url=$url").readText()
+    private fun String.onlyLettersAndDigits(): String = this.filter { it.isLetterOrDigit() }
+
+    private fun getTinyUrl(url: String?): String {
+        // URL("https://urlz.fr/api_new.php?url=$url").readText()
+
+        return HttpClient.newHttpClient().send(
+            HttpRequest.newBuilder()
+                .uri(URI.create("https://zdh.fr/"))
+                .POST(HttpRequest.BodyPublishers.ofString("$url"))
+                .build(),
+            HttpResponse.BodyHandlers.ofString()
+        ).body()
+    }
 
     @EventHandler
     fun onEpisodesRelease(event: EpisodesReleaseEvent) {
@@ -31,7 +47,6 @@ class EpisodesRelease(private val twitter: Twitter) : Listener {
                 )
 
                 val statusUpdate = StatusUpdate("\uD83C\uDF89 ${episode.anime?.name}\n" +
-                        "${episode.title?.ifBlank { "＞﹏＜" } ?: "＞﹏＜"}\n" +
                         "Saison ${episode.season} • ${
                             when (episode.episodeType?.name) {
                                 "SPECIAL" -> "Spécial"
@@ -45,7 +60,8 @@ class EpisodesRelease(private val twitter: Twitter) : Listener {
                                 else -> ""
                             }
                         }\n" +
-                        "URL : ${getTinyUrl(episode.url)}"
+                        "URL : ${getTinyUrl(episode.url)}\n" +
+                        "#Anime #${episode.platform?.name?.onlyLettersAndDigits()} #${episode.anime?.name?.onlyLettersAndDigits()}"
                 )
                 statusUpdate.setMediaIds(media.mediaId)
                 tweets.updateStatus(statusUpdate)
